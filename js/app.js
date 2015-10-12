3816071008f0bc2c7201b29fe460293efc9088b5
Dennis Drenner <dennis@dennisdrenner.com>
2015-10-12 09:11:34 -0400

    Cleaned up app.js code and added more documentation to code. Cleaned up index.html

--- js/app.js
+++ js/app.js
@@ -3,12 +3,11 @@
 
 /* ----------------------- MODEL ----------------------- */
 
-//Constructor function for Location objects (i.e. locations in Hampden)
+//Constructor function for Location objects (i.e. locations in my neighborhood)
 
 var Location = function (data) {
     var self = this; 
     this.name = data.name;
-    //this.imgSrc = ko.observable(data.imgSrc);
     this.city = "Baltimore"; 
     this.state = "Maryland";
     this.zipcode = 21211; 
@@ -16,7 +15,6 @@ var Location = function (data) {
     this.street = data.street;
     this.categories = data.categories;  //categories is an array 
     this.display = ko.observable(true); 
-    //this.placeName = '';
     this.latLng = {};
     this.marker = {};
     this.marker.setMap = '';
@@ -31,11 +29,12 @@ var Location = function (data) {
         + self.city + ", " + self.state + " " + self.zipcode;
     };
 
+    //Marker animation. Bounce after click 
     self.bounce = function () {
       self.marker.setAnimation(google.maps.Animation.BOUNCE);
       window.setTimeout(function () {
         self.marker.setAnimation(null);
-      }, 2000);
+      }, 1000);
     }   
 };
 
@@ -84,14 +83,26 @@ var locations = [
   },
 
   {
-          name: "Random House",
-          streetNumber: "3600",
-          street: "Falls Road",
-          categories: ["home"],
-          summary: "Potential meth lab",
-          yelpId: ""
+          name: "Old Bank Barbers",
+          streetNumber: "1100",
+          street: "W. 36th St",
+          categories: ["barber"],
+          summary: "Barber shop",
+          yelpId: "old-bank-barbers-baltimore"
 
   },
+
+
+  {
+          name: "The Charmery",
+          streetNumber: "801",
+          street: "W. 36th Street",
+          categories: ["ice cream"],
+          summary: "Ice Cream",
+          yelpId: "the-charmery-baltimore"
+
+  },
+
 ]
 
 
@@ -101,8 +112,6 @@ var locations = [
 function AppViewModel() {
 
     var self = this; 
-
-    this.searchBox =  ko.observable("Enter search text");
 
     //List of all locations in the model
     self.locationObjList = ko.observableArray([]);
@@ -118,11 +127,11 @@ function AppViewModel() {
         getYelpData(locationObj);
     });
 
+    this.searchBox =  ko.observable("Enter search text");
    
     //Find locations which are a match for input search text 
     self.displayLocation = ko.computed(function() {
       for (i=0; i<self.locationObjList().length;i++){
-        //console.log(self.locationObjList()[i].flickrURL());
 
         //if searchBox text is a match for part of the name, set display == true on location object
         if (self.locationObjList()[i].name.search(self.searchBox()) !== -1) {
@@ -130,9 +139,8 @@ function AppViewModel() {
           //marker.setMap does not exist when map is first initialized, so we run this check to 
           //avoid errors, 
            if (self.locationObjList()[i].marker.setMap) { self.locationObjList()[i].marker.setMap(map); }
-
         }
-        //else set display = false and remove marker from the map 
+        //else set display = false and remove marker from the map
         else { 
           self.locationObjList()[i].display(false);
             if (self.locationObjList()[i].marker.setMap) { self.locationObjList()[i].marker.setMap(null); }
@@ -141,26 +149,26 @@ function AppViewModel() {
     });
       
 
-    //set up data for google map object defined below 
+    //Set up data for google map object defined below 
     var mapOptions = {
       center: { lat: 39.332769, lng: -76.635661},
       zoom: 16
       };
 
-    //define new google map object 
+    //Define a new google map object 
     var map = new google.maps.Map(document.getElementById('hampdenMap'), mapOptions);
     
+
     //Iterate through locationObjList (array of all location objects), pull out the addresses and use these
     //to calculate latLng info from Google, create a new map marker, and add latLng and marker as properties
-    // to the location object 
+    //to the location object 
 
     function mapMaker() {
-
       var infoDiv = document.getElementById("infoDiv");
 
       for (var x = 0; x < self.locationObjList().length; x++) {
-        // TO DO: Use forEach instead of loop to implement mapMaker function 
-
+  
+        //By calling $.getJSON this way (function(x) { ...} (x)  we lock the current value of x into a closure
         (function (x) {
           $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+self.locationObjList()[x].address()+
                 '&sensor=false', null, function (data) {
@@ -173,6 +181,10 @@ function AppViewModel() {
                         position: latLng,
                         map: map, 
                   });
+
+                  //Event listener on marker injects info on the location into the infoDiv
+                  //Also sets selected marker to green after changing all markers to red 
+                  //(to remove old green markers from previously selected locations)
 
                   marker.addListener('click', function() {
                     infoDiv.innerHTML = "<p> SUMMARY: " + self.locationObjList()[x].summary + "</p>"+
@@ -188,11 +200,11 @@ function AppViewModel() {
                     //Change color of selected marker to green 
                     marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');  
                     
-                    //Marker will bounce for 2 seconds when clicked             
+                    //Marker will bounce for 1 seconds when clicked             
                     marker.setAnimation(google.maps.Animation.BOUNCE);
                     window.setTimeout(function () {
                       marker.setAnimation(null);
-                    }, 2000);                  
+                    }, 1000);                  
                   });
                  
                   //attach latLng and marker to location objects as properties
@@ -207,10 +219,6 @@ function AppViewModel() {
 
     };
 
-   
-
-
-
 
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
@@ -223,10 +231,8 @@ ko.applyBindings(new AppViewModel());
 
 
 
-/**
- * Generates a random number and returns it as a string for OAuthentication
- * @return {string}
- */
+
+ //Generates a random number and returns it as a string for OAuthentication
 
 function nonce_generate() {
   return (Math.floor(Math.random() * 1e12).toString());
@@ -237,15 +243,18 @@ function nonce_generate() {
 
 function getYelpData (locationObj) {
 
+//TO DO: HIDE THIS INFO
   var YELP_KEY = 'a0d6iLsmo3UQwIFD3vQy4g'; 
   var YELP_TOKEN = 'Af0MT-f7yuN1H1SHnecbpbZtYb9nOaIB';
   var YELP_KEY_SECRET = '8-2woIQShndzD2NkVim2ji_VXck';
   var YELP_TOKEN_SECRET = 'yXv1Uc7SeI4eAEw4xUqaq_ncDI0';
 
+//If no Yelp ID exists, exit out of the function. Yelp ID is manually input, not all locations have a presence on Yelp
    if (locationObj.yelpId == "") { 
     return;
    };
 
+//Construct URL for Yelp API call.  OAuth library will add signature to this later
   var yelp_url = 'https://api.yelp.com/v2/business/' + locationObj.yelpId;
 
   var parameters = {
@@ -255,9 +264,10 @@ function getYelpData (locationObj) {
     oauth_timestamp: Math.floor(Date.now()/1000),
     oauth_signature_method: 'HMAC-SHA1',
     oauth_version : '1.0',
-    callback: 'cb'              // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
+    callback: 'cb'    // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
   };
 
+//Use oauth-signature.js library to create encoded signature. 
   var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, YELP_KEY_SECRET, YELP_TOKEN_SECRET);
   parameters.oauth_signature = encodedSignature;
 
@@ -266,116 +276,22 @@ function getYelpData (locationObj) {
     data: parameters,
     cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
     dataType: 'jsonp',
-    success: function(results) {
-      console.log(results);
+    success: function(results) {  //Results is the object passed back by Yelp after the API call. Log it to see
+      //other useful info you can use in your map. 
+      //console.log(results);   
       locationObj.img_url = results.image_url; 
       locationObj.snippet_text = results.snippet_text;
       locationObj.rating_img_url = results.rating_img_url; 
-      //console.log("RESULT FROM AJAX QUERY:", results);
-      //console.log(results.snippet_text);
-      // Do stuff with results
     },
     error: function(response) {
-            // Do stuff on fail
       console.log("THERE WAS AN ERROR!", response);
-
     }
 
     
   };
 
-
-  console.log(encodedSignature);
   // Send AJAX query via jQuery library.
   $.ajax(settings);
 
-}
-
-
-
-
-
-   //Interate through locationObjList and calculate URL
-    // for searching Flickr API for photos of location (returns info for one image as JSON)
-    //Attach this URL to the location object.
-//     self.locationObjList().forEach(function (locationObj) {
-//         var flickrURL = 
-//         "https://api.flickr.com/services/rest/?method=flickr.photos.search" + 
-//         "&api_key=757b8b4527c93ac33eb36984d673ce93" +
-//         "&tags=" + locationObj.name + 
-//         "&safe_search=1&content_type=1&per_page=1&format=json&nojsoncallback=1";
-//         //console.log("FURL--", flickrURL);
-
-
-//         //Get pictureURL from Flickr and attach to location object 
-//         $.getJSON(flickrURL, function(data) {
-//           console.log(data);
-//           // locationObj.photoURL = "https://farm" + data['photos']['photo'][0].farm + 
-//           // ".staticflickr.com/" + data['photos']['photo'][0].server + "/" + data['photos']['photo'][0].id +
-//           // "_" + data['photos']['photo'][0].secret + ".jpg";
-//           //console.log('helloooo', flickrURL, locationObj, locationObj.photoURL);
-//         })
-//         .fail(function( jqxhr, textStatus, error ) {
-//     var err = textStatus + ", " + error;
-//     console.log( "Request Failed: " + err );
-// })
-
-//         // .fail(function() {
-//         //   console.log("ERROR!!");
-//         // })
-//         .always(function() {
-//           console.log("Request completed");
-//         });
-
-        // var response = $.ajax(flickrURL);
-
-        // for (i = 0; i < response.length; i++) {
-        //   console.log(response[i])
-        // };
-
-        // console.log(response);
-        // console.log($.ajax(flickrURL));
-        // console.log($.ajax(flickrURL));
-
-        //console.log(locationObj, locationObj.photoURL);
-
-
-        // var photoURL = "https://farm" + data['photos']['photo'][0].farm + 
-        // ".staticflickr.com/" + data['photos']['photo'][0].server + "/" + "data['photos']['photo'][0].id" +
-    //     // "_" + data['photos']['photo'][0].secret + ".jpg";
-
-
-    // });
-
-       // $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+self.locationObjList()[x].address()+
-       //          '&sensor=false', null, function (data) {
-       //            var p = data.results[0].geometry.location;
-       //            var latLng = new google.maps.LatLng(p.lat, p.lng);
-       //            var marker = 
-       //              new google.maps.Marker({
-       //                  animation: google.maps.Animation.DROP,
-       //                  position: latLng,
-       //                  map: map, 
-       //            });
-
-
-    // //return a URL which will search the Flickr api for a photo matching self.placeName
-    // self.flickrURL = function () {
-      // var URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search" + 
-      // "&api_key=757b8b4527c93ac33eb36984d673ce93" +
-      // "&tags=" + self.placeName + "%2Cbaltimore" +
-      // "&safe_search=1&content_type=1&per_page=1&format=json&nojsoncallback=1" 
-      //"&auth_token=72157659226565246-57e3dceb871c1352" +
-      //"&api_sig=0e6fe25b75dd75a44b023abf07728298";
-    //   //console.log("PLACE name -- " , self.placeName);
-    //   return URL; 
-    // };
-
-    //List of addresses only for use in the calculating Google map markers 
-    //self.addressList = ko.observableArray([]);
-
-    //Iterate through locationList, adding addresses to address list 
-    // self.locationObjList().forEach(function(locationObj) {
-    //     self.addressList.push(locationObj.address());
-    // });    
+}  //End of getYelpData Function 
 
