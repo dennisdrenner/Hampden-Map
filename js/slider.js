a7c20c1c0daebcfa1e4b6768be55109a556d7be0
Dennis Drenner <dennis@dennisdrenner.com>
2015-11-02 07:40:25 -0500

    Tweaked and annotated slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
 
-
     //stick in the fixed 100% height behind the navbar but don't wrap it
-    $('#slide-nav.navbar .container').append($('<div id="navbar-height-col"></div>'));
+    //$('#slide-nav.navbar .container').append($('<div id="navbar-height-col"></div>'));
 
     // Enter your ids or classes
     var toggler = '.navbar-toggle';
@@ -14,10 +13,14 @@ $(document).ready(function () {
     var slideneg = '-80%';
 
 
+    //This function toggles the 'slide-active' class on and off on all page elements when 
+    //the toggler (hamburger menu) is clicked   
     $("#slide-nav").on("click", toggler, function (e) {
 
-        var selected = $(this).hasClass('slide-active');
+        var selected = $(this).hasClass('slide-active');  //true if slide-active class is present on toggler 
 
+
+        //The next four functions trigger 'animation' of slidemenu when clicking toggler  
         $('#slidemenu').stop().animate({
             left: selected ? menuneg : '0px'
         });
@@ -34,16 +37,14 @@ $(document).ready(function () {
             left: selected ? '0px' : slidewidth
         });
 
-
         $(this).toggleClass('slide-active', !selected);
-        $('#slidemenu').toggleClass('slide-active');
 
+        $('#slidemenu').toggleClass('slide-active');
 
         $('#page-content, .navbar, body, .navbar-header').toggleClass('slide-active');
 
 
     });
-
 
     var selected = '#slidemenu, #page-content, body, .navbar, .navbar-header';
 
@@ -54,10 +55,7 @@ $(document).ready(function () {
             $(selected).removeClass('slide-active');
         }
 
-
     });
-
-
 
 
 });
